0dbbf5acb45063788fd4aa811c039537c0184f73
stevenkwokwk <125413337+stevenkwokwk@users.noreply.github.com>
2023-12-03 12:31:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-// Declare the data as a separate variable
 var chartData = [
     ['Age', 'AIA 自願醫保標準計劃', '安聯醫療保障'],
     [0, 2758.4, 4584],
@@ -17,10 +16,14 @@ var chartData = [
     [10, 1568, 1464]
 ];
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable(chartData);
 
-    var options = {
+function drawChart() {
+    var selectedPlans = getSelectedPlans();
+    var filteredData = filterData(chartData, selectedPlans);
+
+    var data = google.visualization.arrayToDataTable(filteredData);
+
+   var options = {
         title: 'Insurance Premiums by Age',
         hAxis: {title: 'Age'},
         vAxis: {title: 'Premium'},
@@ -33,6 +36,38 @@ function drawChart() {
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('line_chart'));
-
     chart.draw(data, options);
 }
+
+function getSelectedPlans() {
+    var selectBox = document.getElementById('planSelect');
+    var selectedPlans = [];
+    for (var i = 0; i < selectBox.options.length; i++) {
+        if (selectBox.options[i].selected) {
+            selectedPlans.push(selectBox.options[i].value);
+        }
+    }
+    return selectedPlans;
+}
+
+function filterData(data, selectedPlans) {
+    var filteredData = [];
+    filteredData.push(data[0]); // Add headers
+
+    for (var i = 1; i < data.length; i++) {
+        var row = [data[i][0]]; // Add age
+        if (selectedPlans.includes("AIA")) {
+            row.push(data[i][1]);
+        } else {
+            row.push(null);
+        }
+        if (selectedPlans.includes("AnLian")) {
+            row.push(data[i][2]);
+        } else {
+            row.push(null);
+        }
+        filteredData.push(row);
+    }
+    return filteredData;
+}
+
