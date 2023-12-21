4765f28a1dd5a8e9444412d5d643e1fec8dcb2a4
stevenkwokwk <125413337+stevenkwokwk@users.noreply.github.com>
2023-12-21 22:41:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getSelectedPlans() {
     var selectedPlanIndices = [];
     for (var i = 0; i < selectBox.options.length; i++) {
         if (selectBox.options[i].selected) {
-            selectedValue=selectBox.options[i].value;
+            selectedValue=int(selectBox.options[i].value);
             selectedPlanIndices.push(selectedValue); // Push the index of the selected plan
         }
     }
